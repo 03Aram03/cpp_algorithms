ce34bce69c7c98e1c99272b32a7761dc54c7549c
03Aram03 <aramhayrapetyan0303@gmail.com>
2025-09-24 22:18:55 +0400

    Create Aram_insertion_sort.cpp

---
+++ Task1/Algorithms/Aram_insertion_sort.cpp
@@ -0,0 +1,40 @@
+//insertion sort
+#include <iostream>
+const int size=10;
+
+int main() {
+
+int arr[size]={2,4,1,3,6,7,5,10,15,9};
+int i=1,j=0,key;
+
+std::cout<<"unsorrted array"<<std::endl;
+
+
+for(i=0;i<size;i++)
+{
+    std::cout<<arr[i]<<"  ";
+}
+std::cout<<std::endl<<std::endl;
+
+
+for(i=1;i<size;i++)
+{
+    key=arr[i];
+    j=i-1;
+    while(j>=0 && arr[j]>=key)
+    {
+        arr[j+1]=arr[j];
+        j-=1;
+    }
+    arr[j+1]=key;
+}
+std::cout<<"sorrted array"<<std::endl;
+
+for(i=0;i<size;i++)
+{
+    std::cout<<arr[i]<<"  ";
+}
+
+
+    return 0;
+}
